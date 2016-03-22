c03ecd007e275d665334e079be6ef132c4b8da2e
Kochanov Dmitry <drakula13@tut.by>
2016-03-22 14:46:45 +0300

    modify init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,2 @@
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -fs /home/box/web/etc/nginx.conf 
 sudo nginx -t && sudo /etc/init.d/nginx restart
-sudo ln -fs /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
