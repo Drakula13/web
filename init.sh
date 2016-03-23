21bc27f2b5ce8f7cca5974450725863d3632b776
Kochanov Dmitry <drakula13@tut.by>
2016-03-23 16:21:28 +0300

    1911

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 sudo ln -fs /home/box/web/etc/nginx.conf 
 sudo nginx -t && sudo /etc/init.d/nginx restart
+sudo ln -fs /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart gunicorn -b 0.0.0.0:8080 hello
