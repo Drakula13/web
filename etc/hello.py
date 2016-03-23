aca70cecbec8a8cb8d2519832089d9dd82087139
Kochanov Dmitry <drakula13@tut.by>
2016-03-23 16:40:15 +0300

    123

--- etc/hello.py
+++ etc/hello.py
@@ -1,2 +1,10 @@
-bind = "0.0.0.0:8080"
-workers = 5
+CONFIG = {
+	'mode': 'wsgi'
+	'working_dir': '/home/box/web/hello.py'
+	'args': (
+		'--bind=0.0.0.0:8080',
+		'--workers=16',
+		'--timeout=60',
+		'hello.app'
+	),
+}
